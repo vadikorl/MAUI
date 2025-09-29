ee2b520dfc106f7d3eeb2794c2fabf8719db5330
vadikorl <vadiraj115@gmail.com>
2025-09-28 20:02:45 -0400

    Updated

--- MauiApp1/obj/Debug/net9.0-ios/iossimulator-arm64/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net9.0-ios/iossimulator-arm64/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+34e513bfe4249b06f121efac858418b1134afb3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
